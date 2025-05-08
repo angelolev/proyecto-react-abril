f988d9a06ae6651dfd22a5a41b79f621f7e9e867
Angelo Leva <angelokta7@gmail.com>
2025-05-07 19:48:48 -0500

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,12 @@ function App() {
   return (
     <main>
       <section className="new-arrivals">
-        <h2>New Arrivals</h2>
-        {products.map((product: ProductCardProps) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+        <h2 className="prueba">New Arrivals</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {products.map((product: ProductCardProps) => (
+            <ProductCard key={product.id} {...product} />
+          ))}
+        </div>
       </section>
     </main>
   );
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -5,11 +5,13 @@ function ProductCard({ title, price, image }: ProductCardProps) {
   return (
     <div className="product-card">
       <div className="product-image">
-        <img src={image} alt="Producto" />
+        <img className="max-w-[150px]" src={image} alt="Producto" />
       </div>
       <div className="product-info">
-        <h3>{title}</h3>
-        <p>${price}</p>
+        <h3 className="text-[20px] font-bold whitespace-nowrap overflow-hidden text-ellipsis max-w-[300px] w-[300px]">
+          {title}
+        </h3>
+        <p className="text-[24px] font-bold">${price}</p>
       </div>
     </div>
   );
